508259f345e63d9cbd78744dc73db0e05a30dcf0
brayhoward <brandonrhoward@gmail.com>
2018-02-14 11:57:07 -0600

    Add test and logic for making private functions available when in :dev

--- lib/private.ex
+++ lib/private.ex
@@ -47,6 +47,9 @@ defmodule Private do
     end
   end
 
+  defp do_private(block, _env = :dev) do
+    make_defs_public(block)
+  end
           
   defp do_private(block, _env = :test) do
     make_defs_public(block)
